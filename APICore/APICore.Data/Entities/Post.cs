1e1a1b2c7a806b449ff5330d887f70dedd46637c
Kevin <kevinfdez41@gmail.com>
2023-02-15 07:22:05 -0600

    feat: Post class created

---
+++ APICore/APICore.Data/Entities/Post.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace APICore.Data.Entities
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Text { get; set; }
+    }
+}
