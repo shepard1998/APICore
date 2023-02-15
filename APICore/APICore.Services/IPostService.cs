434aa412b6d98533242855a26b4936c7ef2f9742
Kevin <kevinfdez41@gmail.com>
2023-02-15 07:27:23 -0600

    feat: added IPostService and PostService

---
+++ APICore/APICore.Services/IPostService.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using APICore.Common.DTO.Request;
+using APICore.Data.Entities;
+
+namespace APICore.Services
+{
+    public interface IPostService
+    {
+        Task<IQueryable<Post>> GetPostAsync(int userId = 0);
+        Task<IQueryable<Post>> GetPostByUserSerialAsync(string serialUser);
+        Task AddPostAsync(AddPostRequest postRequest, int currentUser);
+        Task<Post> UpdatePostAsync (UpdatePostRequest postRequest);
+        Task DeletePostAsync(DeletePostRequest deletePostRequest);
+    }
+}
---
+++ APICore/APICore.Services/Impls/PostService.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using APICore.Common.DTO.Request;
+using APICore.Data.Entities;
+using APICore.Data.UoW;
+using APICore.Services.Exceptions;
+using Microsoft.Extensions.Localization;
+
+namespace APICore.Services.Impls
+{
+    public class PostService : IPostService
+    {
+        private readonly IUnitOfWork _uow;
+
+        private readonly IStringLocalizer<IAccountService> _localizer;
+
+        public PostService(IUnitOfWork uow, IStringLocalizer<IAccountService> localizer)
+        {
+            _uow = uow ?? throw new ArgumentNullException(nameof(uow));
+            _localizer = localizer ?? throw new ArgumentNullException(nameof(localizer));
+        }
+
+        public async Task<IQueryable<Post>> GetPostAsync(int userId=0)
+        {
+            var result = _uow.PostRepository.GetAll();
+
+            if (userId != 0)
+            {
+                result = result.Where(l => l.UserId == userId);
+            }
+
+            return await Task.FromResult(result);
+        }
+
+        public async Task<IQueryable<Post>> GetPostByUserSerialAsync(string serialUser)
+        {
+            var user = await _uow.UserRepository.FirstOrDefaultAsync(u => u.Identity == serialUser);
+
+            if (user == null)
+            {
+                throw new UserNotFoundException(_localizer);
+            }
+
+            var result = GetPostAsync(user.Id);
+
+            return await result;
+        }
+
+        public async Task AddPostAsync(AddPostRequest postRequest, int currentUser)
+        {
+            var post = new Post();
+            post.Text = postRequest.Text;
+            post.CreatedAt = DateTime.Now;
+            post.UserId = currentUser;
+
+            await _uow.PostRepository.AddAsync(post);
+            await _uow.CommitAsync();
+        }
+
+        public async Task<Post> UpdatePostAsync(UpdatePostRequest postRequest)
+        {
+            var post = await _uow.PostRepository.FirstOrDefaultAsync(u => u.Id == postRequest.Id);
+
+            if (post == null)
+            {
+                throw new PostNotFoundException(_localizer);
+            }
+
+            post.Text = postRequest.Text;
+
+            _uow.PostRepository.Update(post);
+            await _uow.CommitAsync();
+
+            return post;
+        }
+
+        public async Task DeletePostAsync(DeletePostRequest deletePostRequest)
+        {
+            var post = await _uow.PostRepository.FirstOrDefaultAsync(u => u.Id == deletePostRequest.Id);
+
+            if (post == null)
+            {
+                throw new PostNotFoundException(_localizer);
+            }
+            
+            _uow.PostRepository.Delete(post);
+            await _uow.CommitAsync();
+        }
+    }
+}
