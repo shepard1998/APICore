631757bf782a143a4f9b3589de853a22dc8c7387
Kevin <kevinfdez41@gmail.com>
2023-02-15 07:30:45 -0600

    feat: added PostNotFoundException

---
+++ APICore/APICore.Services/Exceptions/NotFound/PostNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Localization;
+
+namespace APICore.Services.Exceptions
+{
+    public class PostNotFoundException : BaseNotFoundException
+    {
+        public PostNotFoundException(IStringLocalizer<object> localizer)
+        {
+            CustomCode = 404004;
+            CustomMessage = localizer.GetString(CustomCode.ToString());
+        }
+    }
+}
